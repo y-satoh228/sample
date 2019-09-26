cede09b5f193ed6ea628828d5791d1874ead1ff0
”佐藤雄紀” <y.satoh228@gmail.com>
2019-09-26 10:11:04 +0000

    kadai 3

--- practice.php
+++ practice.php
@@ -39,3 +39,41 @@ $calender_2018 = [
  echo $calender_2018 ["December"]; 
 
 ?>
+
+<?php
+  $name = "ゆうき";
+  if($name =="ゆう") {
+      echo "私は{$name}です";
+  }else{
+      echo "あなたの名前ではありません";
+      echo "\n";
+  }
+
+?>
+<?php
+  $total = 0;
+  
+  for($i = 0; $i <= 10000; $i++) {
+      $total += $i;
+  }
+  echo $total;
+?>
+
+<?php
+  $fruits = array("apple","orange","peach","mango","berry");
+  foreach($fruits as $fruits) {
+  echo $fruits;
+  echo "\n";
+  }
+?>
+<?php
+  $start = 1;
+  $end = 100;
+  
+  for($i = $start; $i <= $end; $i++){
+      if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
+?>
